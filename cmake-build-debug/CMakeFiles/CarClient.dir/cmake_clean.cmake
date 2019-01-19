2c3904741fd28a1c63718113d7d32f71a10de12d
WhuBooks <2016206190028@whu.edu.cn>
2019-01-19 16:37:47 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/CarClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CarClient.dir/main.cpp.o"
+  "CarClient.pdb"
+  "CarClient"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CarClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/Test/CMakeFiles/PubClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PubClient.dir/PubClient.cpp.o"
+  "../../bin/PubClient.pdb"
+  "../../bin/PubClient"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PubClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
